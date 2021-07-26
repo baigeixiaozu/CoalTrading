7bf3dc94a744cf46f7abeab21fcd09b2529289d2
jiyeme <jiyecafe@gmail.com>
2021-07-26 22:00:34 +0800

    update: 框架构建（无实际功能）。

---
+++ src/main/java/cn/coal/trading/controller/UserController.java
@@ -0,0 +1,14 @@
+package cn.coal.trading.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author jiyec
+ * @Date 2021/7/26 21:50
+ * @Version 1.0
+ **/
+@CrossOrigin
+@RestController
+public class UserController {
+}
---
+++ src/main/java/cn/coal/trading/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package cn.coal.trading.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import java.util.Map;
+
+/**
+ * @Author jiyec
+ * @Date 2021/7/26 21:36
+ * @Version 1.0
+ **/
+public interface UserMapper extends BaseMapper <Map>{
+}
