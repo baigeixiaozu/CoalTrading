fd35ee077004e099d7c2d24c783e8e257b536398
Heming233 <heming7159@163.com>
2021-07-27 10:31:49 +0800

    NewsController.java

---
+++ src/main/java/cn/coal/trading/controller/NewsController.java
@@ -0,0 +1,9 @@
+package cn.coal.trading.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin//解决跨域请求授权问题
+@RestController//注释该类为Controller类，
+public class NewsController {
+}
