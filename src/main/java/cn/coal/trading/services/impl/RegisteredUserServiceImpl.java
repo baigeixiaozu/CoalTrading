1357bdb13b6b64899cf412ee83f0cd14168eba22
Sorakado ao <kaga1i@outlook.com>
2021-07-29 11:12:33 +0800

    修改控制层luoji

--- src/main/java/cn/coal/trading/services/impl/RegisteredUserServiceImpl.java
+++ src/main/java/cn/coal/trading/services/impl/RegisteredUserServiceImpl.java
@@ -1,8 +1,10 @@
 package cn.coal.trading.services.impl;
 
 
+import cn.coal.trading.bean.BaseUser;
 import cn.coal.trading.bean.RegisteredUser;
 import cn.coal.trading.mapper.RegisteredUserMapper;
+import cn.coal.trading.mapper.UserMapper;
 import cn.coal.trading.services.RegisteredUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,12 +22,20 @@ import java.util.Map;
 public class RegisteredUserServiceImpl implements RegisteredUserService {
     @Autowired
     public RegisteredUserMapper registeredUserMapper;
+    @Autowired
+    public UserMapper userMapper;
+    @Autowired
+    public BaseUser baseUser;
+
     @Override
     public Map<String,Object> register(RegisteredUser registeredUser) {
         HashMap<String, Object> map = new HashMap<>();
+        baseUser.setLogin(registeredUser.getLogin());
+        baseUser.setPass(registeredUser.getPass());
+        baseUser.setNick(registeredUser.getNick());
+        baseUser.setEmail(registeredUser.getEmail());
 
-
-        int result  = registeredUserMapper.insert(registeredUser);
+        int result  =userMapper.insert(baseUser);
 
        if(result==1){
             map.put("code","201");
