b9a1326482a5152c5875b6a228401e2902b368d9
Heming233 <heming7159@163.com>
2021-07-29 16:38:02 +0800

    News.java NewsController.java

--- src/main/java/cn/coal/trading/bean/News.java
+++ src/main/java/cn/coal/trading/bean/News.java
@@ -1,5 +1,6 @@
 package cn.coal.trading.bean;
 
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,12 +16,16 @@ Version:v1.0
  */
 
 @Data//配置Setter、Getter方法
-@Component//将pojo类实例化到spring容器内
+/*@Component//将pojo类实例化到spring容器内*/
 @NoArgsConstructor//配置无参构造方法
 @AllArgsConstructor//配置有参构造方法
+@TableName(value="ct_news")//配置对应数据库的表
 public class News implements Serializable {
-    private int newsID;//资讯编号
+    private String newsID;//资讯编号
     private String newTitle;//资讯标题
     private String newsContent;//资讯内容
     private Date newDate;//资讯日期
+    private int status;//状态：草稿：1；发布：2
+    private String authorID;//发布人员ID
+    private String auditorID;//审核人员ID
 }
