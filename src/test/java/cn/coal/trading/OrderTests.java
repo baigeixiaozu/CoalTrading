57fcd1bd93ba2ec11879c4329e1055193c3c6f54
Heming233 <heming7159@163.com>
2021-08-09 19:20:35 +0800

    OrderController.java OrderService.java OrderServiceImpl.java OrderTests.java

---
+++ src/test/java/cn/coal/trading/OrderTests.java
@@ -0,0 +1,25 @@
+package cn.coal.trading;
+
+import cn.coal.trading.bean.Order;
+import cn.coal.trading.services.OrderService;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by Heming233
+ * Date:2021/8/9
+ * version:v1.0
+ */
+
+@SpringBootTest
+public class OrderTests {
+    @Resource
+    OrderService orderService;
+
+    @Test
+    void addOrder(){
+        Order order=orderService.addNewOrder(1L,2L);
+    }
+}
