95a731117752b5135438a1bff8d33d6245acfed3
Heming233 <heming7159@163.com>
2021-08-11 11:57:35 +0800

    NewsServiceImpl.java

--- src/main/java/cn/coal/trading/services/impl/NewsServiceImpl.java
+++ src/main/java/cn/coal/trading/services/impl/NewsServiceImpl.java
@@ -87,7 +87,7 @@ public class NewsServiceImpl implements NewsService {
         //设置查询条件
         wrapper.like("title",title);
         wrapper.eq("status","4");
-        wrapper.select("title","id");
+        wrapper.select("title","id","date");
 
         return newsMapper.selectList(wrapper);
     }
