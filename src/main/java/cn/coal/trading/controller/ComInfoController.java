79e7539c93100b3e82730679363784b888707038
Sorakado ao <kaga1i@outlook.com>
2021-08-12 22:25:19 +0800

    更新

--- src/main/java/cn/coal/trading/controller/ComInfoController.java
+++ src/main/java/cn/coal/trading/controller/ComInfoController.java
@@ -5,6 +5,11 @@ import cn.coal.trading.bean.ResponseData;
 import cn.coal.trading.mapper.CompanyMapper;
 import cn.coal.trading.services.impl.FileServiceImpl;
 import com.baomidou.shaun.core.annotation.HasRole;
+import com.github.xiaoymin.knife4j.annotations.ApiSupport;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -14,6 +19,9 @@ import java.util.List;
 @HasRole("USER_REG_AUDITOR")
 @RestController
 @RequestMapping("/info")
+
+@Api(tags = "企业信息模块")
+@ApiSupport(author = "songyan.bai")
 public class ComInfoController {
     @Resource
     private CompanyMapper companyMapper;
@@ -21,6 +29,7 @@ public class ComInfoController {
     FileServiceImpl fileService;
     // 不能把响应体写在此处
 
+    @ApiOperation(value = "basicInfo",notes = "显示待审核信息")
     @GetMapping("/{id}")//显示信息
     public ResponseData basicInfo(@PathVariable Long id) {
         ResponseData responseData = new ResponseData();
@@ -38,8 +47,9 @@ public class ComInfoController {
         return responseData;
     }
 
+    @ApiOperation(value = "opinion",notes = "提交审核意见")
     @GetMapping("/{id}/{opinion}")//提交审核意见
-    public ResponseData Opinion(@PathVariable Long id,@PathVariable String opinion){
+    public ResponseData opinion(@PathVariable Long id,@PathVariable String opinion){
         Boolean flag=companyMapper.Opinion(id, opinion);
         ResponseData responseData = new ResponseData();
         if(!flag){
@@ -53,7 +63,9 @@ public class ComInfoController {
             responseData.setError("无");
         }
         return responseData;
-    }//审核意见
+    }
+    //审核意见
+    @ApiOperation(value = "verify",notes = "确认按钮")
     @GetMapping("/verify/{id}")
     public ResponseData verify(@PathVariable Long id){
         Boolean flag=companyMapper.verify(id);
@@ -69,7 +81,9 @@ public class ComInfoController {
             responseData.setError("无");
         }
         return responseData;
-    }//确认按钮
+    }
+    //确认按钮
+    @ApiOperation(value = "reject",notes = "未通过按钮")
     @GetMapping("/reject/{id}")
     public ResponseData reject(@PathVariable Long id){
 
@@ -87,6 +101,7 @@ public class ComInfoController {
         }
         return responseData;
     }//未通过按钮
+    @ApiOperation(value = "download",notes = "下载图片or显示")
     @GetMapping("/download/{id}/{name}")//下载，不是图片显示，在这里不实现
     public ResponseData download(@PathVariable Long id,@PathVariable String name) throws IOException {//文件下载，不采用
         String down = companyMapper.download(id, name);
